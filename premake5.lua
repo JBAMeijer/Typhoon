fa2beccafab90ecc115b83a26aa17ad6239fe89a
Joey Meijer <meijerjoey@gmail.com>
2020-11-21 21:35:12 +0100

    Event System

--- premake5.lua
+++ premake5.lua
@@ -14,7 +14,7 @@ project "Hazel"
 
 	files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
 
-	includedirs {"%{prj.name}/vendor/spdlog/include"}
+	includedirs {"%{prj.name}/src", "%{prj.name}/vendor/spdlog/include"}
 
 	filter "system:windows"
 		cppdialect "C++17"
