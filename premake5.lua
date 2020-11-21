1b8bfd40e21bdcefb91f9845de01ed6a43e727d6
Joey Meijer <meijerjoey@gmail.com>
2020-11-21 23:25:54 +0100

    Added abstractwindow and windowswindow

--- premake5.lua
+++ premake5.lua
@@ -4,6 +4,12 @@ workspace "Hazel"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
+
+include "Hazel/vendor/GLFW"
+
 project "Hazel"
 	location "Hazel"
 	kind "SharedLib"
@@ -15,18 +21,40 @@ project "Hazel"
 	pchheader "hzpch.h"
 	pchsource "Hazel/src/hzpch.cpp"
 
-	files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
+	files 
+	{
+		"%{prj.name}/src/**.h", 
+		"%{prj.name}/src/**.cpp"
+	}
 
-	includedirs {"%{prj.name}/src", "%{prj.name}/vendor/spdlog/include"}
+	includedirs 
+	{
+		"%{prj.name}/src", 
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
+
+	links
+	{
+		"GLFW",
+		"opengl32.lib"
+	}
 
 	filter "system:windows"
 		cppdialect "C++17"
 		staticruntime "On"
 		systemversion "latest"
 
-		defines {"HZ_PLATFORM_WINDOWS", "HZ_BUILD_DLL"}
+		defines 
+		{
+			"HZ_PLATFORM_WINDOWS", 
+			"HZ_BUILD_DLL"
+		}
 
-		postbuildcommands {("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")}
+		postbuildcommands 
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
 
 	filter "configurations:Debug"
 		defines {"HZ_DEBUG"}
@@ -48,9 +76,17 @@ project "Sandbox"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
+	files 
+	{
+		"%{prj.name}/src/**.h", 
+		"%{prj.name}/src/**.cpp"
+	}
 
-	includedirs {"Hazel/vendor/spdlog/include", "Hazel/src"}
+	includedirs 
+	{
+		"Hazel/vendor/spdlog/include", 
+		"Hazel/src"
+	}
 
 	links {"Hazel"}
 
