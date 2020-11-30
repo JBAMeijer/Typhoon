7a7cbf7f6d0bfc3e590337961f231356e775632d
Joey Meijer <meijerjoey@gmail.com>
2020-11-30 22:17:50 +0100

    Added OrthographicCamera

--- Hazel/src/Hazel/Renderer/Shader.h
+++ Hazel/src/Hazel/Renderer/Shader.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <glm/glm.hpp>
 
 namespace Hazel
 {
@@ -13,6 +14,7 @@ namespace Hazel
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
 
+		virtual void UploadUniformMat4(const std::string& name, const glm::mat4& matrix) = 0;
 		static Shader* Create(const std::string& vertexSrc, const std::string& fragmentSrc);
 	};
 
