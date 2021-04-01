af9a7848998abe475e7b887ea2d76fa64739bc51
Joey Meijer <meijerjoey@gmail.com>
2021-04-01 21:13:30 +0200

    Added platform checks to base header and updated premake

--- Typhoon/src/Typhoon/Core/Base.h
+++ Typhoon/src/Typhoon/Core/Base.h
@@ -2,19 +2,62 @@
 
 #include <memory>
 
+// Platform detection
+#ifdef _WIN32
+	/* Windows x64/x86 */
+	#ifdef _WIN64
+		/* Windows x64 */
+		#define TYPH_PLATFORM_WINDOWS
+	#else
+		/* Windows x86 */
+		#error "32-bit builds are not supported!"
+	#endif
+#elif defined(__APPLE__) || defined(__MAC__)
+	#include <TargetConditionals>
+	/* The TARGET_OS_MAC macro exists on all apple related platforms.
+	 * We must first check the platform specific ones before checking this one
+	 * to ensure that we're running on a MAC and not some other Apple platform */
+	#if TARGET_IPHONE_SIMULATOR == 1
+		#error "IOS simulator is not supported!"
+	#elif TARGET_OS_IPHONE == 1
+		#define TYPH_PLATFORM_IOS
+		#error "IOS is not supported!"
+	#elif TARGET_OS_MAC == 1
+		#define TYPH_PLATFORM_MACOS
+		#error "MacOS is not supported!"
+	#else
+		#error "Unknown Apple platform!"
+	#endif
+/* We also have to check the __ANDROID__ macro before __linux__ 
+   Since android is based on the Linux kernel it also has the
+   __linux__ macro defined */
+#elif defined(__ANDROID__)
+	#define TYPH_PLATFORM_ANDROID
+	#error "Android is not supported!"
+#elif defined(__linux__)
+	#define TYPH_PLATFORM_LINUX
+	#error "Linux is not supported!"
+#else
+	/* Unknown platform */
+	#error "Unknown platform!"
+#endif 
+// End of platform detection
+
+// DLL support
 #ifdef TYPH_PLATFORM_WINDOWS
-#if TYPH_DYNAMIC_LINK
-	#ifdef TYPH_BUILD_DLL
-		#define TYPHOON_API __declspec(dllexport)
-	#else 
-		#define TYPHOON_API __declspec(dllimport)
+	#if TYPH_DYNAMIC_LINK
+		#ifdef TYPH_BUILD_DLL
+			#define TYPHOON_API __declspec(dllexport)
+		#else 
+			#define TYPHOON_API __declspec(dllimport)
+		#endif
+	#else
+		#define TYPHOON_API
 	#endif
 #else
-	#define TYPHOON_API
-#endif
-#else
-#error Typhoon only supports windows!
-#endif
+	#error Typhoon only supports windows!
+#endif 
+// End of DLL support
 
 #ifdef TYPH_DEBUG
 	#define TYPH_ENABLE_ASSERTS
