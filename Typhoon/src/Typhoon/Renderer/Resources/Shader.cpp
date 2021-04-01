db3b86e398a251ede5439a8bf06344dc569495eb
Joey Meijer <meijerjoey@gmail.com>
2021-04-01 21:46:19 +0200

    Replaced raw pointers with scopes

--- Typhoon/src/Typhoon/Renderer/Resources/Shader.cpp
+++ Typhoon/src/Typhoon/Renderer/Resources/Shader.cpp
@@ -46,21 +46,21 @@ namespace Typhoon {
 		m_Shaders[name] = shader;
 	}
 
-	Typhoon::Ref<Typhoon::Shader> ShaderLibrary::Load(const std::string& filepath)
+	Ref<Shader> ShaderLibrary::Load(const std::string& filepath)
 	{
 		auto shader = Shader::Create(filepath);
 		Add(shader);
 		return shader;
 	}
 
-	Typhoon::Ref<Typhoon::Shader> ShaderLibrary::Load(const std::string& name, const std::string& filepath)
+	Ref<Shader> ShaderLibrary::Load(const std::string& name, const std::string& filepath)
 	{
 		auto shader = Shader::Create(filepath);
 		Add(name, shader);
 		return shader;
 	}
 
-	Typhoon::Ref<Typhoon::Shader> ShaderLibrary::Get(const std::string& name)
+	Ref<Shader> ShaderLibrary::Get(const std::string& name)
 	{
 		TYPH_CORE_ASSERT(Exists(name), "Shader not found!");
 		return m_Shaders[name];
