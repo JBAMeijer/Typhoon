e9af9aa022ab142821efda2607685bd12db017e4
Joey Meijer <meijerjoey@gmail.com>
2021-04-01 21:26:11 +0200

    Fixed edge case in OpenGLShader::PreProcess Logic

--- Typhoon/src/Platform/OpenGL/OpenGLShader.cpp
+++ Typhoon/src/Platform/OpenGL/OpenGLShader.cpp
@@ -121,18 +121,19 @@ namespace Typhoon
 
 		const char* typeToken = "#type";
 		size_t typeTokenLength = strlen(typeToken);
-		size_t pos = source.find(typeToken, 0);
+		size_t pos = source.find(typeToken, 0); // Start of shader type declaration
 		while (pos != std::string::npos)
 		{
-			size_t eol = source.find_first_of("\r\n", pos);
+			size_t eol = source.find_first_of("\r\n", pos); // End of shader type declaration
 			TYPH_CORE_ASSERT(eol != std::string::npos, "Syntax error");
-			size_t begin = pos + typeTokenLength + 1;
+			size_t begin = pos + typeTokenLength + 1; // Start of shader type name 
 			std::string type = source.substr(begin, eol - begin);
 			TYPH_CORE_ASSERT(ShaderTypeFromString(type), "Invalid shader type specified!");
 
-			size_t nextLinePos = source.find_first_not_of("\r\n", eol);
+			size_t nextLinePos = source.find_first_not_of("\r\n", eol); // Start of shader code after shader type declaration
+			TYPH_CORE_ASSERT(nextLinePos != std::string::npos, "Syntax error");
 			pos = source.find(typeToken, nextLinePos);
-			shaderSources[ShaderTypeFromString(type)] = source.substr(nextLinePos, pos - (nextLinePos == std::string::npos ? source.size() - 1 : nextLinePos));
+			shaderSources[ShaderTypeFromString(type)] = (pos == std::string::npos) ? source.substr(nextLinePos) : source.substr(nextLinePos, pos - nextLinePos);
 		}
 
 		return shaderSources;
