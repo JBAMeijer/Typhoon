6b06f90310088dc5eeb90993e09507adf9a90f66
Joey Meijer <meijerjoey@gmail.com>
2021-04-02 17:15:12 +0200

    Added file reading check

--- Typhoon/src/Platform/OpenGL/OpenGLShader.cpp
+++ Typhoon/src/Platform/OpenGL/OpenGLShader.cpp
@@ -127,10 +127,18 @@ namespace Typhoon
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
-			result.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
-			in.close();
+			size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(size);
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], size);
+				in.close();
+			}
+			else
+			{
+				TYPH_CORE_ERROR("Could not read from file '{0}'", filepath);
+			}
 		}
 		else
 		{
